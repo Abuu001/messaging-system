86c25145dad29a318577a8748f8897804eaa1dfa
steveokay <mokaysteve@gmail.com>
2021-03-12 23:08:32 +0300

    Refactor: renaming our services

---
+++ Upload-service/src/test/java/com/trimeo/Uploadservice/UploadServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trimeo.Uploadservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
