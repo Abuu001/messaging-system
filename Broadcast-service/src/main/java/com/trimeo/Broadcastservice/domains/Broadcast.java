50507d1c44d7dd1c80ee4f0ce72a11221c0de186
Steve Okoth <Steve.Okoth@ncbagroup.com>
2021-03-17 16:29:24 +0300

    broadcast: adding broadcast domain and broadcast repository to enable crud fro broadcast table

---
+++ Broadcast-service/src/main/java/com/trimeo/Broadcastservice/domains/Broadcast.java
@@ -0,0 +1,36 @@
+package com.trimeo.Broadcastservice.domains;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Data
+@Document("broadcast")
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Broadcast {
+
+    @Id
+    private Integer _id;
+
+    private String clientCode;
+
+    private String dateCreated;
+
+    private String dateModified;
+
+    private String expiryTime;
+
+    private List<Integer> listIDs;
+
+    private String sendTime;
+
+    private String sourceAddress;
+
+    private Integer createdBy;
+
+    private String message;
+
+}
--- Broadcast-service/src/main/java/com/trimeo/Broadcastservice/repositories/BroadcastRepository.java
+++ Broadcast-service/src/main/java/com/trimeo/Broadcastservice/repositories/BroadcastRepository.java
@@ -1,9 +1,9 @@
 package com.trimeo.Broadcastservice.repositories;
 
-import com.trimeo.Broadcastservice.domains.User;
+import com.trimeo.Broadcastservice.domains.Broadcast;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface BroadcastRepository extends CrudRepository<Broadcast, Integer> {
 }
