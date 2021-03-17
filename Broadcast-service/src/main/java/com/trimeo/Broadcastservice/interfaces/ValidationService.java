8a5c06fe75810dfb6f782b32baec59d5b028eade
steveokay <mokaysteve@gmail.com>
2021-03-17 19:27:59 +0300

    broadcast: adding validation service

--- Broadcast-service/src/main/java/com/trimeo/Broadcastservice/interfaces/ValidationService.java
+++ Broadcast-service/src/main/java/com/trimeo/Broadcastservice/interfaces/ValidationService.java
@@ -1,4 +1,12 @@
 package com.trimeo.Broadcastservice.interfaces;
 
+import com.trimeo.Broadcastservice.dtos.BroadcastDTO;
+
 public interface ValidationService {
+
+    boolean validateBroadcast(BroadcastDTO broadcastDTO);
+    boolean notExpired(BroadcastDTO broadcastDTO);
+    boolean shortCodeActiveAndExist(String shortCode);
+    boolean checkIfMessageActive(BroadcastDTO broadcastDTO);
+
 }
--- Broadcast-service/src/main/java/com/trimeo/Broadcastservice/services/ValidationServiceImpl.java
+++ Broadcast-service/src/main/java/com/trimeo/Broadcastservice/services/ValidationServiceImpl.java
@@ -1,12 +1,35 @@
 package com.trimeo.Broadcastservice.services;
 
+import com.trimeo.Broadcastservice.dtos.BroadcastDTO;
 import com.trimeo.Broadcastservice.interfaces.ValidationService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
+import javax.validation.Valid;
+
 @Slf4j
 @Service
 @Validated
 public class ValidationServiceImpl implements ValidationService {
+
+    @Override
+    public boolean validateBroadcast(@Valid BroadcastDTO broadcastDTO) {
+        return false;
+    }
+
+    @Override
+    public boolean notExpired(@Valid BroadcastDTO broadcastDTO) {
+        return false;
+    }
+
+    @Override
+    public boolean shortCodeActiveAndExist(String shortCode) {
+        return false;
+    }
+
+    @Override
+    public boolean checkIfMessageActive(@Valid BroadcastDTO broadcastDTO) {
+        return false;
+    }
 }
