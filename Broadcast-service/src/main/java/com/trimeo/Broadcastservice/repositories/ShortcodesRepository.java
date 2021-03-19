d08191daa7275987012d720aa2cf98ccd068321e
steveokay <mokaysteve@gmail.com>
2021-03-19 11:56:00 +0300

    broadcast: annotating shortcodesRepository

--- Broadcast-service/src/main/java/com/trimeo/Broadcastservice/repositories/ShortcodesRepository.java
+++ Broadcast-service/src/main/java/com/trimeo/Broadcastservice/repositories/ShortcodesRepository.java
@@ -2,9 +2,11 @@ package com.trimeo.Broadcastservice.repositories;
 
 import com.trimeo.Broadcastservice.domains.Shortcodes;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ShortcodesRepository extends PagingAndSortingRepository<Shortcodes, Integer> {
     Optional<Shortcodes> findByName(String name);
 }
