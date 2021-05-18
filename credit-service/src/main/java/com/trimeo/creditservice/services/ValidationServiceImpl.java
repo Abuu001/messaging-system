c41d3d68f8e13975705ac5a390c731c14f1d7f95
steveokay <mokaysteve@gmail.com>
2021-05-18 09:00:27 +0300

    credit-service: validation class cleanup

--- credit-service/src/main/java/com/trimeo/creditservice/services/ValidationServiceImpl.java
+++ credit-service/src/main/java/com/trimeo/creditservice/services/ValidationServiceImpl.java
@@ -1,6 +1,5 @@
 package com.trimeo.creditservice.services;
 
-import com.trimeo.creditservice.domains.Credits;
 import com.trimeo.creditservice.interfaces.validationService;
 import com.trimeo.creditservice.repositories.ClientCreditRepository;
 import lombok.NonNull;
@@ -8,15 +7,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class ValidationServiceImpl implements validationService {
 
     @NonNull
-    private ClientCreditRepository clientCreditRepository;
+    private final ClientCreditRepository clientCreditRepository;
 
     @Override
     public boolean clientHasEnoughCredits(String clientCode) {
@@ -26,13 +23,13 @@ public class ValidationServiceImpl implements validationService {
     @Override
     public boolean clientExists(String clientCode) {
 
-        Optional<Credits> credits = clientCreditRepository.findByClientCode(clientCode);
-
-        if(credits.isPresent()){
-            //
-        }else{
-            log.error("");
-        }
+//        Optional<Credits> credits = clientCreditRepository.findByClientCode(clientCode);
+//
+//        if(credits.isPresent()){
+//            //
+//        }else{
+//            log.error("");
+//        }
 
         return false;
     }
