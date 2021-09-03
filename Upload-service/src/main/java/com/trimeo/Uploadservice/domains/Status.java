2c34d82a2ce6e53e4fa3485f399eff464da6cf57
Abuu001 <abulugz@gmail.com>
2021-09-03 22:45:29 +0300

    upload-service : adding domains

---
+++ Upload-service/src/main/java/com/trimeo/Uploadservice/domains/Status.java
@@ -0,0 +1,5 @@
+package com.trimeo.Uploadservice.domains;
+
+public enum Status {
+    ACTIVE,INACTIVE
+}
---
+++ Upload-service/src/main/java/com/trimeo/Uploadservice/domains/Upload.java
@@ -0,0 +1,8 @@
+package com.trimeo.Uploadservice.domains;
+
+public class Upload {
+    private Integer id;
+    private String filename;
+    private String sourceAddress;
+    private Status status;
+}
