4118ddc231fdc3fd953d7a1a1cf6de433614e714
Abuu001 <abulugz@gmail.com>
2021-09-20 23:46:37 +0300

    upload-service : changed genval to identity

--- Upload-service/src/main/java/com/trimeo/Uploadservice/domains/UploadMessage.java
+++ Upload-service/src/main/java/com/trimeo/Uploadservice/domains/UploadMessage.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 public class UploadMessage {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @NotNull(message = "destination cannot be empty")
