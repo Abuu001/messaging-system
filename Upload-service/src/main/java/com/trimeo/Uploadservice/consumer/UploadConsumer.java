0b2f6500a138f0295ea55a5aba94c60233901e52
Abuu001 <abulugz@gmail.com>
2021-10-01 23:25:03 +0300

    upload : md

--- Upload-service/src/main/java/com/trimeo/Uploadservice/consumer/UploadConsumer.java
+++ Upload-service/src/main/java/com/trimeo/Uploadservice/consumer/UploadConsumer.java
@@ -5,7 +5,6 @@ import com.trimeo.Uploadservice.domains.OutboundPayload;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -18,13 +17,12 @@ public class UploadConsumer {
    /* @Value("${upload.rabbit.upload_queue}")
     private  final String _UPLOAD_QUEUE;*/
 
-    @RabbitListener(queues = "INBOUNDUPLOAD.QUEUE")
+     @RabbitListener(queues = "INBOUNDUPLOAD.QUEUE")
     public void consumerInboundUploadMessageQueue(List<InboundUploadMessage> queuePayload){
 
         log.info("::::::::::::::::::::::: 🚀🚀🚀 UPLOAD-INBOUND-PAYLOAD received succseefully 🚀🚀🚀::::::::::::::::::::::");
         log.info(String.valueOf(queuePayload)+ "\n \n");
     }
-
 
     @RabbitListener(queues = "OUTBOUNDUPLOAD.QUEUE")
     public void consumerOutboundUploadMessageQueue(List<OutboundPayload> queuePayload){
