f2057672ea1159b5558e1a55e5661cd6ebda19b0
Abuu001 <abulugz@gmail.com>
2021-10-01 23:20:24 +0300

    upload : endpoint to fetch file name

--- Upload-service/src/main/java/com/trimeo/Uploadservice/controller/FileUploadController.java
+++ Upload-service/src/main/java/com/trimeo/Uploadservice/controller/FileUploadController.java
@@ -1,15 +1,13 @@
 package com.trimeo.Uploadservice.controller;
 
-import com.trimeo.Uploadservice.repository.UploadRepository;
+import com.trimeo.Uploadservice.amqp.BatchConfigProperties;
 import com.trimeo.Uploadservice.service.FileDataReaderImp;
 import com.trimeo.Uploadservice.validator.FileAvailability;
 import com.trimeo.Uploadservice.validator.FileValidator;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,30 +21,27 @@ import java.io.IOException;
 @Slf4j
 public class FileUploadController {
 
-   @Value("${upload.PROPERTY_EXCEL_SOURCE_FILE_PATH}")
-    private  String PROPERTY_EXCEL_SOURCE_FILE_PATH;
+ /*  @Value("${upload.batch-files-config.PROPERTY_EXCEL_SOURCE_FILE_PATH}")
+    private  String PROPERTY_EXCEL_SOURCE_FILE_PATH;*/
+    @Autowired
+    private BatchConfigProperties batchConfigProperties;
 
     @Autowired
-    FileDataReaderImp fileDataReaderImp;
+    private FileDataReaderImp fileDataReaderImp;
 
     @Autowired
-    FileValidator fileValidator;
+    private  FileValidator fileValidator;
 
     @Autowired
-    FileAvailability isFileavailable;
-
-    @Autowired
-    RabbitTemplate rabbitTemplate;
-
-    @Autowired
-    UploadRepository uploadRepository;
-
+    private FileAvailability isFileavailable;
 
     @GetMapping("upload/file")
     public String saveFileUploadedData() throws IOException {
 
-         if(isFileavailable.fileExists(PROPERTY_EXCEL_SOURCE_FILE_PATH)){
-             fileValidator.checkIfFileIsValid(PROPERTY_EXCEL_SOURCE_FILE_PATH);
+         if(isFileavailable.fileExists(batchConfigProperties.getPROPERTY_SOURCE_FILE_PATH())){
+             fileValidator.checkIfFileIsValid(batchConfigProperties.getPROPERTY_SOURCE_FILE_PATH());
+             fileDataReaderImp.saveFileDetails(batchConfigProperties.getPROPERTY_SOURCE_FILE_PATH());
+             fileDataReaderImp.saveUploadDataToQueue();
              return "File Uploaded Sucessfully 👻👻👻👻";
          }
 
